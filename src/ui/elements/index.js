7c5bb4447c9875f3b52da317fc2715cd6982fa92
Piotr F <hello@piotrf.pl>
2020-04-27 19:57:08 +0200

    Export elements and settings.

--- src/ui/elements/index.js
+++ src/ui/elements/index.js
@@ -1,3 +1,6 @@
-export { default as CreditsIcon } from './Icons/CreditsIcon';
-export { default as PointerIcon } from './Icons/PointerIcon';
-export { default as ShareIcon } from './Icons/ShareIcon';
+export { default as AlignCenterHorizontalIcon } from './icons/AlignCenterHorizontalIcon';
+export { default as AlignLeftIcon } from './icons/AlignRightIcon';
+export { default as AlignRightIcon } from './icons/AlignLeftIcon';
+export { default as CreditsIcon } from './icons/CreditsIcon';
+export { default as PointerIcon } from './icons/PointerIcon';
+export { default as ShareIcon } from './icons/ShareIcon';
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,2 +1,4 @@
 export * from './components';
+export * from './elements';
+export * from './settings';
 export { default as docTheme } from './theme';
